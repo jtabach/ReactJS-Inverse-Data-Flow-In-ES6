fc18a5172638fd9be77a28697c755d933ec7bb16
jtabach <jeff.n.tabachnick@gmail.com>
2016-04-11 16:39:18 -0700

    bundles components:

---
+++ src/components/PostWrapper.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class PostWrapper extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import PostWrapper from './components/PostWrapper';
+
+ReactDom.render(
+  <PostWrapper />
+  ,
+  document.querySelector('.container')
+);
