fe4c43f897649cf4aef2d7929ad6cc69173239d1
jtabach <jeff.n.tabachnick@gmail.com>
2016-04-11 18:01:40 -0700

    imports PostInput and PostList

--- src/components/PostWrapper.js
+++ src/components/PostWrapper.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import PostInput from './PostInput';
+import PostList from './PostList';
+
 export default class PostWrapper extends Component {
   constructor(props) {
     super(props);
