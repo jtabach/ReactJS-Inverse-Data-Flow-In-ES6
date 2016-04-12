422829364c055a0a1a17719f533509f4f85c2af1
jtabach <jeff.n.tabachnick@gmail.com>
2016-04-11 18:00:30 -0700

    includes child components in PostWrapper

--- src/components/PostWrapper.js
+++ src/components/PostWrapper.js
@@ -4,9 +4,11 @@ export default class PostWrapper extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
-      <div>Hello</div>
-    )
+      <PostInput />
+      <PostList />
+    );
   }
 }
